019a4d43f8dcb714b3249d2a7d4c6e700a9b87be
Petr Arsentev <Proton1987@yandex.ru>
2020-05-04 02:19:54 +0300

    "Сумма и разность чисел со Scanner"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        int result;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        int a = scanner.nextInt();
+        System.out.println("Введите второе число");
+        int b = scanner.nextInt();
+        System.out.println("Теперь нажмите 1 чтобы получить сумму");
+        System.out.println("либо 2 чтобы получить разность");
+        int c = scanner.nextInt();
+        if (c == 1){
+            result = a + b;
+            System.out.println("Сумма чисел = " + result );
+        }
+        if (c == 2){
+            result = a - b;
+            System.out.println("Разница чисел = " + result);
+        }
+
+
     }
 }

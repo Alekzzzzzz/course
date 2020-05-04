4f41a69f6c7c0a3c67bf6fd7f6044506d43f5e7a
Petr Arsentev <Proton1987@yandex.ru>
2020-05-05 01:48:26 +0300

    "фибоначи два способа"
    # Conflicts:
    #	src/com/github/alekzzzzzz/course/Fibonacci.java

--- src/com/github/alekzzzzzz/course/Fibonacci.java
+++ src/com/github/alekzzzzzz/course/Fibonacci.java
@@ -7,22 +7,34 @@ public class Fibonacci {
 
     public static void main(String[] args) {
 
-        System.out.println(fibo(10));
+        System.out.println(fibo2(10));
     }
 
-    private static int  fibo(int a) {
+    private static int  fibo1(int i) {
+        if (i == 0) {
+            return 0;
+        }
+        if (i == 1) {
+            return  1;
+        } else {
+            return fibo1(i - 2) + fibo1(i - 1);
+        }
+
+    }
+
+    private static int fibo2(int a) {
         int first = 0;
         int next = 1;
         int courent = 0;
         System.out.println(first);
         System.out.println(next);
-        for (int i = 1; i < 10;i++){
+        for (int i = 1; i < a; i++){
             courent = first + next;
             first = next;
             next = courent;
             System.out.println(courent);
-
         }
         return courent;
     }
+
 }

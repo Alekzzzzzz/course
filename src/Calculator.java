89db5c77871bfec958e648c2052c315d844512d3
Petr Arsentev <Proton1987@yandex.ru>
2020-05-04 14:35:56 +0300

    "Калькулятор конструктор"

---
+++ src/Calculator.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    /*
+     * Создаем конструкор калькулятор
+     */
+     Calculator() {
+        Scanner scanner = new Scanner(System.in);
+        MathOperations mathOperations = new MathOperations();
+        double result = 0;
+        System.out.println("Введите первое число");
+        int a = scanner.nextInt();
+        System.out.println("Введите второе число");
+        int b = scanner.nextInt();
+        System.out.println("Теперь выберите команду ");
+        System.out.println("1 чтобы получить сумму");
+        System.out.println("2 чтобы получить разность");
+        System.out.println("3 чтобы получить частное");
+        System.out.println("4 чтобы получить произведение");
+        int c = scanner.nextInt();
+        if (c == 1){
+          result =  mathOperations.summa(a,b);
+            System.out.println("Сумма чисел = " + result);
+        }
+        if (c == 2){
+            result = mathOperations.raznost(a,b);
+            System.out.println("Разница чисел = " + result);
+        }
+        if (c == 3){
+          result =  mathOperations.chastnoe(a,b);
+            System.out.println("Частное чисел = " + result);
+        }
+        if (c == 4){
+          result =  mathOperations.proizvedenie(a,b);
+            System.out.println("Произведение чисел = " + result);
+        }
+         System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+    }
+}
---
+++ src/MathOperations.java
@@ -0,0 +1,22 @@
+/**
+ * Класс выполняющий математические действия
+ */
+public class MathOperations {
+
+
+    double summa(double a, double b) {
+        return a + b;
+    }
+
+    double raznost(double a, double b) {
+        return a - b;
+    }
+
+    double chastnoe(double a, double b) {
+        return a / b;
+    }
+
+    double proizvedenie(double a, double b) {
+        return a * b;
+    }
+}
